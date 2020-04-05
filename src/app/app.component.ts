6172eb5daa1f993f77445d03299d66f39f1b935c
Chris <coopertime08@gmail.com>
2020-04-05 17:57:04 -0500

    .\angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NG421wk1day1ToDoApp';
+  title = 'Todos';
+  todoList: any[] = [];
+  todoTitle: string;
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit() {
+    this.todoTitle = '';
+    this.todoList = [{ title: 'Install Angular CLI', isDone: false, todoDate: new Date()}];
+  }
+
+   // adds a todo to our list
+   addTodo(): void {
+    this.todoList.push({
+      title: this.todoTitle,
+      isDone: false,
+      todoDate: Date.now()
+    });
+    // resets our todoTitle variable to an empty string
+    this.todoTitle = '';
+  }
+
+  // a method to delete an item
+  deleteTodo(todo: any) {
+    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+    this.todoList.splice(index, 1);
+  }
+
+  // a method to cross out an item
+  completed(todo: any) {
+    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+    this.todoList[index].isDone = !this.todoList[index].isDone;
+    console.log(this.todoList[index].isDone);
+  }
+
+
 }
