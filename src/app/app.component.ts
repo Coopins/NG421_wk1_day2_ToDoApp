cc162bfcebb3827e57075724fa46e8ff666018f3
Chris <coopertime08@gmail.com>
2020-04-05 18:10:00 -0500

    .\src\app\app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,30 +11,29 @@ export class AppComponent {
   todoList: any[] = [];
   todoTitle: string;
 
+
   // tslint:disable-next-line: use-lifecycle-interface
   ngOnInit() {
     this.todoTitle = '';
     this.todoList = [{ title: 'Install Angular CLI', isDone: false, todoDate: new Date()}];
   }
 
-   // adds a todo to our list
+
    addTodo(): void {
     this.todoList.push({
       title: this.todoTitle,
       isDone: false,
       todoDate: Date.now()
     });
-    // resets our todoTitle variable to an empty string
+
     this.todoTitle = '';
   }
 
-  // a method to delete an item
   deleteTodo(todo: any) {
     const index = this.todoList.findIndex(todoItem => todoItem === todo);
     this.todoList.splice(index, 1);
   }
 
-  // a method to cross out an item
   completed(todo: any) {
     const index = this.todoList.findIndex(todoItem => todoItem === todo);
     this.todoList[index].isDone = !this.todoList[index].isDone;
